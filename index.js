5be119fb5d6f178ec49cd75173743c241ae60c02
vitoria <vitoria.dev.f@gmail.com>
2022-12-30 00:46:55 -0300

    [add] fix layout

---
+++ index.js
@@ -0,0 +1,60 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteTitulo = document.getElementById("quote-titulo");
+const quoteText = document.getElementById("quote-text");
+const authorText = document.getElementById("author");
+const shareBtn = document.getElementById("share");
+const newQuote = document.getElementById("loader");
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+//API
+
+async function getQuote() {
+  loading();
+  const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loader, Show Quote
+    complete();
+  } catch (error) {
+    getQuote();
+  }
+}
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+getQuote();
+
+newQuote.addEventListener("click", getQuote);
+shareBtn.addEventListener("click", tweetQuote);
